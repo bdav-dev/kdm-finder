4d3e198e4b69b45dc7f5b908ecde7c6cbf79dd3d
David Berezowski <david.berezowski1@gmail.com>
2024-03-27 13:27:13 +0100

    Added website and GitHub link to info dialog

--- ui/dialogs/info_dialog.py
+++ ui/dialogs/info_dialog.py
@@ -1,6 +1,8 @@
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt6.QtGui import QPixmap, QDesktopServices
+from PyQt6.QtCore import Qt, QUrl
+from ui.widgets.hspacer import HSpacer
+from ui.widgets.vspacer import VSpacer
 from util.file_system_util import get_absolute_path
 from util.ui_util import centered
 
@@ -16,7 +18,21 @@ class InfoDialog(QDialog):
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         appname_label = QLabel("KDM-Finder (Version 0.1)")
-        appname_label.setContentsMargins(0, 0, 0, 10)
+
+        links_layout = QHBoxLayout()
+        links_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        website_link_label = QLabel('<a href="https://www.bdav.dev/code/kdm-finder">Website</a>')
+        website_link_label.setOpenExternalLinks(True)
+        website_link_label.linkActivated.connect(self.open_link)
+       
+        github_link_label = QLabel('<a href="https://github.com/bdav-dev/kdm-finder">GitHub</a>')
+        github_link_label.setOpenExternalLinks(True)
+        github_link_label.linkActivated.connect(self.open_link)
+
+        links_layout.addWidget(website_link_label)
+        links_layout.addItem(HSpacer(10))
+        links_layout.addWidget(github_link_label)
 
         logo_label = QLabel()
         logo_pixmap = QPixmap(get_absolute_path(__file__, "..", "..", "assets", "images", "logo.webp")).scaled(128, 128)
@@ -27,7 +43,13 @@ class InfoDialog(QDialog):
 
         layout.addWidget(centered(logo_label))
         layout.addWidget(centered(appname_label))
+        layout.addItem(VSpacer(7))
+        layout.addLayout(links_layout)
+        layout.addItem(VSpacer(7))
         layout.addWidget(centered(dev_label))
         layout.addWidget(centered(author_label))
 
         self.setLayout(layout)
+
+    def open_link(self, link):
+        QDesktopServices.openUrl(QUrl(link))
